45a0821dc4ff2101a3324707a84ae648cc31df7b
longtran-btc <longtv@btc.studio.io>
2025-02-10 09:33:57 +0700

    #66

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    dangerouslyAllowSVG: true,
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '4000',
+        pathname: '/**',
+      },
+    ],
+  },
+};
 
 export default nextConfig;
